cc0344cca2edc0a42642fd79efad5d895217d126
Ayush Chaurasia <94608049+ayushraut2111@users.noreply.github.com>
2022-04-24 00:47:27 +0530

    Add files via upload

---
+++ including another cpp file in a cpp file.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include "new.h"
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    table(n);
+}
---
+++ new.h
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print()
+{
+    printf("hello");
+}
+void table(int n)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << n << "*" << i << "=" << n * i << endl;
+    }
+}
