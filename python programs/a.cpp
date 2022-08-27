7156a13ddc0607aee071e726fbb0d2abfacebfea
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-08-27 16:58:19 +0530

    a

--- python programs/a.cpp
+++ python programs/a.cpp
@@ -3,8 +3,20 @@
 using namespace std;
 int main()
 {
-   vector<int>v(3);
-   for(auto &x:v)cin>>x;
-   for(auto x:v)
-   cout<<x<<endl;
+   int n,m;
+   cin>>n>>m;
+   vector<int>v1(n),v2(m);
+   for(auto &x:v1)cin>>x;
+   for(auto &x:v2)cin>>x;
+   if(n==m==1)
+   {
+    if(v1[0]==v2[0])
+    {
+        cout<<"Banta"<<endl;
+    }
+    else
+    cout<<"Alice"<<endl;
+   }
+   else
+   cout<<"Alice"<<endl;
 }
