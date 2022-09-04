a6ec41ecdae80b9ae7bf47ba70c82543b4df5e24
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-09-05 02:19:08 +0530

    roy and symmetric logo

--- python programs/roy and symmetric logos.py
+++ python programs/roy and symmetric logos.py
@@ -1,8 +1,21 @@
+def solve(n,a):
+    for i in range(n):
+        for j in range((n//2)+1):
+            if a[i][j]!=a[i][n-1-j]:
+                return False
+    for i in range((n//2)+1):
+        for j in range(n):
+            if a[i][j]!=a[n-1-i][j]:
+                return False
+    return True
+            
 for _ in range(int(input())):
     n=int(input())
     a=[]
     for i in range(n):
-        s=list(map(int,input().split()))[:n]
+        s=list(input())
         a.append(s)
-    
-    
+    if solve(n,a[:]):
+        print("YES")
+    else:
+        print("NO")
