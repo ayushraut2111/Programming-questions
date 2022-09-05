f46492c959cda8038d5499e2685d58601443f47a
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-09-06 02:13:03 +0530

    fun game

--- python programs/fun game.py
+++ python programs/fun game.py
@@ -1,18 +1,34 @@
+from collections import deque as dq
 n=int(input())
 a=list(map(int,input().split()))
 b=a[:]
+a=dq(a)
 ans=[]
-a.reverse()
+# a.reverse()
+# while len(a)>0 and len(b)>0:
+#     if a[len(a)-1]>b[len(b)-1]:
+#         ans.append(1)
+#         b.pop()
+#     elif a[len(a)-1]<b[len(b)-1]:
+#         ans.append(2)
+#         a.pop()
+#     else:
+#         ans.append(0)
+#         a.pop()
+#         b.pop()
+# for i in ans:
+#     print(i,end=" ")
+
 while len(a)>0 and len(b)>0:
-    if a[len(a)-1]>b[len(b)-1]:
+    if a[0]>b[len(b)-1]:
         ans.append(1)
         b.pop()
-    elif a[len(a)-1]<b[len(b)-1]:
+    elif a[0]<b[len(b)-1]:
         ans.append(2)
-        a.pop()
+        a.popleft()
     else:
         ans.append(0)
-        a.pop()
         b.pop()
+        a.popleft()
 for i in ans:
     print(i,end=" ")
