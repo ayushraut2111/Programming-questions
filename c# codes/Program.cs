80142bf219f10fa4e86a0f520331b51a6906b651
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-09-25 00:02:36 +0530

    c#

--- c# codes/Program.cs
+++ c# codes/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.Write("hello");
+﻿class A{
+    public void eat()
+    {
+        Console.Write("dog");
+    }
+}
+class B:A{
+     public void eat()
+    {
+        Console.Write("cat");
+    }
+    public static void Main(){
+        B b1=new B();
+        b1.eat();
+    }
+}
