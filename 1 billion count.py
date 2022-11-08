53051597ce70bbd270331e75b9428de26336e9fb
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-11-09 01:37:03 +0530

    a

--- 1 billion count.py
+++ 1 billion count.py
@@ -2,4 +2,4 @@ import time
 for i in range(1000000000):
     start_time = time.time()
     pass
-print("--- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - start_ti
