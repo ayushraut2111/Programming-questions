293a7d235ebfd1ea8a3493f16403477a6ccf85f9
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-11-09 23:34:20 +0530

    test

--- 1 billion count.py
+++ 1 billion count.py
@@ -2,4 +2,8 @@
 # for i in range(1000000000):
 #     start_time = time.time()
 #     pass
-# print("--- %s seconds ---" % (time.time() - start_ti
+# print("--- %s seconds ---" % (time.time() - start_time)
+a=[1,1]
+i=0
+j=len(a)-1
+print(min(a[i],a[j]))
