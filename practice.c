db75e78550998892483bb44d4e018c0f28322871
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-02-06 14:48:52 +0530

    2 pointer

--- practice.c
+++ practice.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 void pr(int *a)
 {
     for(int i=0;i<3;i++)
@@ -8,6 +9,16 @@ void pr(int *a)
             printf("%d ",*(a+i*3+j));
         }
         printf("\n");
+    }
+}
+void pri(int **a)
+{
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d\n",a[i][j]);
+        }
     }
 }
 void main()
@@ -23,6 +34,17 @@ void main()
     //     }
     // }
     // pr(a);
-    
+    int **a;
+    a=malloc(3*sizeof(int)); //rows=3
+    for (int i=0;i<3;i++)
+    {
+        a[i]=malloc(3*sizeof(int)); //column=3
+        for(int j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    pri(a);
+
 
 }
