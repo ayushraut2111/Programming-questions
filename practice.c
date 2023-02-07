87e14cde62c582d2436f58b384ff030482206ad3
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-02-07 16:20:41 +0530

    dsa

--- practice.c
+++ practice.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
+#include<math.h>
 struct pair{
     int a,b;
 };
 void main()
 {
-    struct pair p[3];
-    for(int i=0;i<3;i++)
-    {
-        int a,b;
-        scanf("%d %d",&a,&b);
-        p[i].a=a;p[i].b=b;
-    }
-    for(int i=0;i<3;i++)
-    {
-        printf("%d %d\n",p[i].a,p[i].b);
-    }
+    // struct pair p[3];
+    // for(int i=0;i<3;i++)
+    // {
+    //     int a,b;
+    //     scanf("%d %d",&a,&b);
+    //     p[i].a=a;p[i].b=b;
+    // }
+    // for(int i=0;i<3;i++)
+    // {
+    //     printf("%d %d\n",p[i].a,p[i].b);
+    // }
+    // float x=pow(2,);
+    // printf("%f",x);
 }
