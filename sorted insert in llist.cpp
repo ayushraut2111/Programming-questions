59fa83d4e52dedb94882b587479e4f52d912299b
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-02-08 16:23:59 +0530

    sort insert

---
+++ sorted insert in llist.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+struct node{
+    int data;
+    node* next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+void print(node* head)
+{
+    while(head!=NULL)
+    {
+        cout<<head->data<<endl;
+        head=head->next;
+    }
+}
+node* insertlast(node* head,int data)
+{
+    node* ptr =new node(data);
+    if(head==NULL)
+    {
+        head=ptr;
+        return head;
+    }
+    node* temp=head;
+    while(temp->next!=NULL)
+    temp=temp->next;
+    temp->next=ptr;
+    return head;
+}
+node* sortinsert(node* head,int data)
+{
+    node* ptr=new node(data);
+    if(head==NULL)
+    {
+        head=ptr;
+        return head;
+    }
+    if (data<=head->data)
+    {
+        ptr->next=head;
+        head=ptr;
+        return head;
+    }
+    node* temp=head,*temp1=head;
+    while(temp!=NULL && data<temp->data)
+    {
+        temp1=temp;
+        temp=temp->next;
+    }
+    ptr->next=temp1->next;
+    temp1->next=ptr;
+    return  head;
+
+}
+int main()
+{
+    node* head=NULL;
+    head=sortinsert(head,7);
+    head=sortinsert(head,1);
+    head=sortinsert(head,3);
+    print(head);
+
+}
