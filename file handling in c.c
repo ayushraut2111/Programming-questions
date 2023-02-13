7e22c493f47951877fcb57168c624b0a68426e56
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-02-13 18:14:34 +0530

    file handling in c

--- file handling in c.c
+++ file handling in c.c
@@ -2,7 +2,10 @@
 int main()
 {
     FILE * fp;
-    fp=fopen("new.txt","w");
-    fprintf(fp,"%s","Hi i am ayush chaurasia");
+    fp=fopen("new.txt","r");
+    // fprintf(fp,"%s","Hi i am ayush chaurasia");
+    char buff[256];
+    fscanf(fp,"%s",buff);
+printf("%s",buff);
     fclose(fp);
 }
