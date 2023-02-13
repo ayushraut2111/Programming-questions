359f35829c9b42f02d833fbd9aa06b70640e0dd6
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-02-13 13:47:58 +0530

    pointers

--- check kth bit is set or not.c
+++ check kth bit is set or not.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
 void main()
 {
-    int x,y;
-    scanf("%d %d",&x,&y);
-    if(x&(1<<y-1)!=0)
-    printf("yes");
-    else
-    printf("no");
-    
+    int a=23;
+    int* ptr=&a;
+    int **x=&ptr;
+    printf("%d",**x);
 }
